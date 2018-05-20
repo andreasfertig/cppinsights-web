c3f759ea726e8f5c4f1a2e3ef733fa29ade1bded
Andreas Fertig <mail@cppinsights.io>
2018-05-20 19:37:39 +0200

    removed unused debug/testing functionality.

--- app/__init__.py
+++ app/__init__.py
@@ -141,19 +141,6 @@ def api():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-# for debugging/testing disable caching
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
 #------------------------------------------------------------------------------
 
 if __name__ == "__main__":
