c3db7a573afe46cd3e2a1fa8513fa220d59c66f5
Andreas Fertig <mail@cppinsights.io>
2018-10-22 19:08:56 +0200

    Cleanup javascript

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,143 +1,144 @@
 /* C++ Insights Web, copyright (c) by Andreas Fertig
    Distributed under an MIT license. See /LICENSE */
-var cppEditor = CodeMirror.fromTextArea(document.getElementById("cpp-code"), {
-    lineNumbers: true,
-    matchBrackets: true,
-    styleActiveLine: true,
-    mode: "text/x-c++src"
+
+/* global form, CodeMirror */
+
+var cppEditor = CodeMirror.fromTextArea(document.getElementById('cpp-code'), {
+  lineNumbers: true,
+  matchBrackets: true,
+  styleActiveLine: true,
+  mode: 'text/x-c++src'
 });
+
 if (window.sessionStorage) {
   cppEditor.focus();
-  var pos = window.sessionStorage.getItem("position");
+  var pos = window.sessionStorage.getItem('position');
   if (pos) {
     try {
       cppEditor.setCursor(JSON.parse(pos));
-    } catch(e) {
+    } catch (e) {
+      // continue, error does not matter here
     }
   }
 }
-var mac = CodeMirror.keyMap.default == CodeMirror.keyMap.macDefault;
-CodeMirror.keyMap.default[(mac ? "Cmd" : "Ctrl") + "-Space"] = "autocomplete";
-var cppOutEditor = CodeMirror.fromTextArea(document.getElementById("cpp-code-out"), {
-    lineNumbers: true,
-    matchBrackets: true,
-    styleActiveLine: true,
-    readOnly: true,
-    mode: "text/x-c++src"
-});
-var stdErrEditor = CodeMirror.fromTextArea(document.getElementById("stderr-out"), {
-    lineNumbers: false,
-    readOnly: true,
-    mode: "shell"
-});
 
-function copyText() {
-    document.getElementById("cpp-code").value = cppEditor.getValue();
-}
+var mac = CodeMirror.keyMap.default == CodeMirror.keyMap.macDefault;
+CodeMirror.keyMap.default[(mac ? 'Cmd' : 'Ctrl') + '-Space'] = 'autocomplete';
+var cppOutEditor = CodeMirror.fromTextArea(document.getElementById('cpp-code-out'), {
+  lineNumbers: true,
+  matchBrackets: true,
+  styleActiveLine: true,
+  readOnly: true,
+  mode: 'text/x-c++src'
+});
+var stdErrEditor = CodeMirror.fromTextArea(document.getElementById('stderr-out'), {
+  lineNumbers: false,
+  readOnly: true,
+  mode: 'shell'
+});
 
 function readSingleFile(e) {
-    var file = e.target.files[0];
-    if (!file) {
-        return;
-    }
-    var reader = new FileReader();
-    reader.onload = function(e) {
-        var contents = e.target.result;
-        displayContents(contents);
-    };
-    reader.readAsText(file);
+  var file = e.target.files[0];
+  if (!file) {
+    return;
+  }
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var contents = e.target.result;
+    displayContents(contents);
+  };
+  reader.readAsText(file);
 }
 
 function displayContents(contents) {
-    /*var element = document.getElementById('cpp-code');
-    element.textContent = contents;*/
-    cppEditor.setValue(contents);
+  cppEditor.setValue(contents);
 }
 
 document.querySelector('.button-upload')
-  .addEventListener('click', function (event) {
+  .addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('file-input').click();
   });
 
 document.getElementById('file-input')
-    .addEventListener('change', readSingleFile);
+  .addEventListener('change', readSingleFile);
 
-document.querySelector('.button-download').addEventListener('click', function (event) {
+document.querySelector('.button-download').addEventListener('click', function(event) {
   event.preventDefault();
-  download("f.txt", cppEditor.getValue());
+  download('f.txt', cppEditor.getValue());
 });
 
 function download(filename, text) {
-    var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    pom.setAttribute('download', filename);
-    if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
-    } else {
-        pom.click();
-    }
+  var pom = document.createElement('a');
+  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  pom.setAttribute('download', filename);
+  if (document.createEvent) {
+    var event = document.createEvent('MouseEvents');
+    event.initEvent('click', true, true);
+    pom.dispatchEvent(event);
+  } else {
+    pom.click();
+  }
 }
 
+/*
 function toggleConsole() {
-    var element = document.getElementById('stderr-div').style
-    if ('none' == element.display) {
-        element.display = 'initial';
-    } else {
-        element.display = 'none';
-    }
+  var element = document.getElementById('stderr-div').style;
+  if ('none' == element.display) {
+    element.display = 'initial';
+  } else {
+    element.display = 'none';
+  }
 }
+*/
 
-//var form = document.getElementById('form');
-//document.body.addEventListener('keydown', function(e) {
 form.addEventListener('keydown', function(e) {
-	if(!((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey)) return;
+  if (!((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey)) return;
   submit();
 });
 
 function submit() {
   if (window.sessionStorage) {
-    window.sessionStorage.setItem("position", JSON.stringify(cppEditor.getCursor()));
+    window.sessionStorage.setItem('position', JSON.stringify(cppEditor.getCursor()));
   }
 
   form.submit();
 }
 
 function CopyClick() {
-  var textToCopy = document.getElementById("lnkurl");
+  var textToCopy = document.getElementById('lnkurl');
 
   textToCopy.select();
 
-  document.execCommand("copy");
+  document.execCommand('copy');
 }
 
-document.querySelector(".button-create-link").addEventListener("click", function(event) {
+document.querySelector('.button-create-link').addEventListener('click', function(event) {
   event.preventDefault();
   event.stopPropagation();
   var hostname = window.location.hostname;
   var text = 'https://' + hostname + '/lnk?code=' + btoa(cppEditor.getValue()) + '&rev=1.0';
 
-  var lnkElement = document.getElementById("lnkurl");
+  var lnkElement = document.getElementById('lnkurl');
   lnkElement.value = text;
 
-  var element = document.getElementById("copyDropdown");
-  element.classList.toggle("show");
+  var element = document.getElementById('copyDropdown');
+  element.classList.toggle('show');
 });
 
-var runButton = document.querySelector(".button-run");
+var runButton = document.querySelector('.button-run');
 if (runButton) {
-  runButton.title = "Run C++ Insights (" + (mac ? "Cmd-Return" : "Ctrl-Enter") + ")";
-  runButton.addEventListener('click', function (event) {
+  runButton.title = 'Run C++ Insights (' + (mac ? 'Cmd-Return' : 'Ctrl-Enter') + ')';
+  runButton.addEventListener('click', function(event) {
     event.preventDefault();
     submit();
   });
 }
+
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn') && !event.target.matches('.cpybtn') && !event.target.matches('#lnkurl')) {
 
-    var dropdowns = document.getElementsByClassName("copyDownDownContent");
+    var dropdowns = document.getElementsByClassName('copyDownDownContent');
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
@@ -146,4 +147,4 @@ window.onclick = function(event) {
       }
     }
   }
-}
+};
